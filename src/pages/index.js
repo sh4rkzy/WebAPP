b0e5e0dfe281e1fc7972166c1584e46196ad735e
Kaue Oliveira <kaueoliveirac11@gmail.com>
2022-12-28 03:13:35 -0300

    Inserção do mailto

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
 
               <div>
                 <a
-                  href="https://www.instagram.com/sh4rkzy__/"
+                  href="mailto:kauecampos01@hotmail.com"
                   className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-sky-200"
                 >
                   <SVGEmail/>
